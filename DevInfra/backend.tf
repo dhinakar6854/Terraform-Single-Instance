fdf2168bcb96b2b469975c8a0f41c8261c77185c
dhinakar6854 <53922730+dhinakar6854@users.noreply.github.com>
2026-03-17 11:55:02 +0530

    Update backend.tf

--- DevInfra/backend.tf
+++ DevInfra/backend.tf
@@ -2,15 +2,13 @@ terraform {
   required_version = "<= 1.4.6" #Forcing which version of Terraform needs to be used
   required_providers {
     aws = {
-      version = "<= 5.0.0" #Forcing which version of plugin needs to be used.
+      version = "~> 6.0" #Forcing which version of plugin needs to be used.
       source  = "hashicorp/aws"
     }
   }
   backend "s3" {
-    bucket         = "sreeterraformbucket"
+    bucket         = "dummy-terraform-functions-bucket"
     key            = "devsecops-modules.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "devsecopsb33b34_terraform_locks"
-    encrypt        = true
-  }
+    }
 }
