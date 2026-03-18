0057a6cd708adea3a3fe512ed87fa9b8fd74e6d9
dhinakar6854 <53922730+dhinakar6854@users.noreply.github.com>
2026-03-18 11:46:17 +0530

    Fix syntax for subnet ID outputs in outputs.tf

--- modules/1.network/outputs.tf
+++ modules/1.network/outputs.tf
@@ -11,15 +11,15 @@ output environment {
 }
 
 output public_subnets_id {
-  value       = aws_subnet.public-subnets.[*].id
+  value       = aws_subnet.public-subnets[*].id
 }
 
 output private_subnets_id {
-  value       = aws_subnet.private-subnets.[*].id
+  value       = aws_subnet.private-subnets[*].id
 }
 
 output public_subnets_id_1 {
-  value       = aws_subnet.public-subnets.[0].id
+  value       = aws_subnet.public-subnets[0].id
 }
 
 output private_route_table_id {
