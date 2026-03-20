ac3fcbb9bd9213165357ee4bb833596c495dbc71
dhinakar6854 <53922730+dhinakar6854@users.noreply.github.com>
2026-03-20 11:38:43 +0530

    Update public_ec2.tf

--- modules/5.compute/public_ec2.tf
+++ modules/5.compute/public_ec2.tf
@@ -13,16 +13,18 @@ resource "aws_instance" "public-servers" {
     Project = "${local.pname}"
   }
     user_data = <<-EOF
-  	#!/bin/bash
-    sudo apt update
-    sudo apt install nginx -y
-    sudo apt install git -y
-    sudo git clone -b DevSecOpsB334 https://github.com/mavrick202/webhooktesting.git
-    sudo rm -rf /var/www/html/index.nginx-debian.html
-    sudo cp webhooktesting/index.html /var/www/html/index.nginx-debian.html
-    sudo cp webhooktesting/style.css /var/www/html/style.css
-    sudo cp webhooktesting/scorekeeper.js /var/www/html/scorekeeper.js
-  	#echo "<div><h1>${var.vpc_name}-Public-Server-${count.index + 1}</h1></div>" >> /var/www/html/index.nginx-debian.html
-    sed -i '29i <center><div id="container"><h1>${var.vpc_name}-Public-Server-${count.index + 1}</h1></div></center>' /var/www/html/index.nginx-debian.html
-  EOF
+   #!/bin/bash
+   apt update -y
+   apt install nginx git -y
+   cd /home/ubuntu
+   git clone https://github.com/dhinakar6854/webhooktesting.git
+   rm -f /var/www/html/index.nginx-debian.html
+   cp /home/ubuntu/webhooktesting/index.html /var/www/html/index.html
+   cp /home/ubuntu/webhooktesting/style.css /var/www/html/style.css
+   cp /home/ubuntu/webhooktesting/scorekeeper.js /var/www/html/scorekeeper.js
+   chown -R www-data:www-data /var/www/html
+   chmod -R 755 /var/www/html
+   systemctl restart nginx
+   sed -i '29i <center><div id="container"><h1>${var.vpc_name}-Public-Server-${count.index + 1}</h1></div></center>' /var/www/html/index.html
+EOF
 }
